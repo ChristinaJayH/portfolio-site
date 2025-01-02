6d9c646e34b59e86983110319366cf0fac18c830
Clifton Heggins <cliftonh2025@gmail.com>
2025-01-02 10:00:53 -0500

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import ServicesPage from './pages/ServicesPage';
-import HomePage from './pages/HomePage';
-import InfoPage from './pages/InfoPage';
-import Navbar from './components/navbar';
-import Footer from './components/Footer';
+
 function App() {
   return (
-  <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route index element={<HomePage/>}/>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/info-page" element={<InfoPage />} />
-        <Route path='/services' element={<ServicesPage/>}/>
-      </Routes>
-    <Footer/>
-    </BrowserRouter>
+<div>
+  
+</div>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,44 +1,10 @@
-import { NavLink } from 'react-router-dom'
-import '../Stylesheets/navbar.css'
-import { useState, useEffect } from 'react';
-import { GiHamburgerMenu } from "react-icons/gi";
-import { FaTimes } from 'react-icons/fa'
-const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-
-
+import './styles/navbar.css';
+const navbar = () => {
   return (
-    <div className={`navbar ${scrolled ? 'scrolled' : ''}`}>
-        <div className="title_Area">
-        PrimeSecOps
-        </div>
-        <div className="hammenu" onClick={() => setMenuOpen(!menuOpen)}>
-        {menuOpen ? <FaTimes  className='hamburger'/> : <GiHamburgerMenu className='exit'/>}
-      </div>
-      <div className="navbar_Links">
-        <ul className={menuOpen ? "open" : ""}>
-          <li><NavLink to='/home' className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Home</NavLink></li>
-          <li><NavLink to='/info-page' className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>About</NavLink></li>
-          <li><NavLink to='/services' className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Services</NavLink></li>
-        </ul>
-      </div>
+    <div>
+      
     </div>
   )
 }
 
-export default Navbar
+export default navbar
