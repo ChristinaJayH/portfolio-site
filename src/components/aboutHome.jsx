042e46aba65201034fc66211a689b7729486e774
Clifton Heggins <cliftonh2025@gmail.com>
2025-01-11 20:14:57 -0500

    m

---
+++ src/components/aboutHome.jsx
@@ -0,0 +1,13 @@
+import '../styles/aboutHome.css'
+const Header = () => {
+  return (
+    <div className='aboutHome'>
+        <div className='aboutHome_title'>
+                <h1>About PrimeSecOps</h1>
+                <p>Learn cybersecurity, build skills, and protect systems for a safer digital future.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,10 @@
-
+import Header from '../components/aboutHome'
 import PrimeSecOps from '../components/primesecops'
 import PrimeInformation from '../components/selections'
 const AboutPage = () => {
   return (
     <div>
+      <Header/>
       <PrimeSecOps/>
       <PrimeInformation/>
     </div>
