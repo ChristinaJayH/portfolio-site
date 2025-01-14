8a05d0ee18e6b0591153d3302ab9b2b251458fcf
Clifton Heggins <cliftonh2025@gmail.com>
2025-01-14 14:33:45 -0500

    lates

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,12 @@
 import '../styles/contact.css'
-import image from '../images/mobile-device.png'
-
 const Contact = () => {
   return (
     <div className='items_container'>
-        <img src={image} alt='mobile device' className='image'/>
-        <h1><span>We're just a call away!</span></h1>
+        <div className='items_content_container'>
+        <h1>We're just a call away!</h1>
         <p>Get in touch with us, and start your CyberSecurity Journey!</p>
         <button className='cta2'>Contact</button>
+        </div>
     </div>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,10 @@
 import '../styles/header.css';
-import { GiCctvCamera } from "react-icons/gi";
 const Header = () => {
   return (
     <div className='header'>
-      <h1>Start Your <span>Cybersecurity</span> <br></br>Journey Today</h1>
-      <p>Embark on a path to learn, grow, and succeed in the world of cybersecurity. Start building the skills you need to advance your career today.</p>
-      <button><GiCctvCamera className='icon'/>Get Started</button>
+      <h1>Start Your Cybersecurity <br></br>Journey Today</h1>
+      <p>Begin your journey to learn, grow, and excel in the field of cybersecurity. Start developing the skills you need to elevate your career today.</p>
+      <button>Get Started</button>
     </div>
   )
 }
